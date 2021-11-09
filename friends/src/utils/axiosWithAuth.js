ba7ce585afd624275a7324cd7f094de9eb03f8a2
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-11-09 14:18:18 -0600

    added axiosWithAuth util

--- friends/src/utils/axiosWithAuth.js
+++ friends/src/utils/axiosWithAuth.js
@@ -1,1 +1,12 @@
 import axios from 'axios'
+
+export const axiosWithAuth = () => {
+  const token = localStorage.getItem('token')
+
+  return axios.create({
+    headers: {
+      authorization: token,
+    },
+    baseURL: 'http://localhost:5000/api',
+  })
+}
