c14ac2c6abfd971898b823a2e271399764420c2e
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-11-09 14:12:35 -0600

    added login form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const initialState = { username: '', password: '' }
 
 const Login = () => {
-  return <h2>Login</h2>
+  const [credentials, setCredentials] = useState(initialState)
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+
+    setCredentials((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }))
+  }
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <label>
+          Username
+          <input
+            type='text'
+            name='username'
+            placeholder='required'
+            value={credentials.username}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Password
+          <input
+            type='password'
+            name='password'
+            placeholder='required'
+            value={credentials.password}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button>Submit</button>
+      </form>
+    </div>
+  )
 }
 
 export default Login
